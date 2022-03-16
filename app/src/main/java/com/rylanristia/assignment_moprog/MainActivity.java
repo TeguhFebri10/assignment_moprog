0e686db06cb91f4cff48090b591276058dc1dacb
Rylanristia <50695667+rylanristia@users.noreply.github.com>
2022-03-16 13:31:08 +0700

    Init commit

---
+++ app/src/main/java/com/rylanristia/assignment_moprog/MainActivity.java
@@ -0,0 +1,14 @@
+package com.rylanristia.assignment_moprog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
